da72ff66c7982a2e439f6b9533cbaa836e3a3881
Wen Rui <wenrui@berkeley.edu>
2017-07-10 00:27:56 -0700

    Trivia Setup

---
+++ src/questions.js
@@ -0,0 +1,118 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "In what year did Singapore achieve independence?": [
+                "1965",
+                "1964",
+                "1945",
+                "1819",
+            ]
+        },
+        {
+            "Who is Singapore's biggest trading partner": [
+                "Malaysia",
+                "China",
+                "Indonesia",
+                "USA",
+                "Canada",
+                "Japan",
+                "South Korea"
+            ]
+        },
+        {
+            "What is the name of Singapore's third Prime Minister?": [
+                "Lee Hsien Loong",
+                "Goh Chok Tong",
+                "Tony Tan",
+                "Lee Kuan Yew"
+            ]
+        },
+        {
+            "What is the name of Singapore's airport?": [
+                "Changi Airport",
+                "Incheon Airport",
+                "Haneda Airport",
+                "Kuala Lumpur International Airport"
+            ]
+        },
+        {
+            "What is the newest MRT(subway) line in Singapore?": [
+                "Downtown Line",
+                "East-West Line",
+                "North-South Line",
+                "Circle Line"
+            ]
+        },
+        {
+            "Which is Singapore's biggest local company?": [
+                "Singapore Airlines",
+                "Creative Technologies",
+                "Razer Inc",
+                "Singtel"
+            ]
+        },
+        {
+            "What are the colors of the Singapore flag?": [
+                "Red and White",
+                "Blue, Red and White",
+                "Yellow, Red, White and Blue",
+                "Yellow, Red and White"
+            ]
+        },
+        {
+            "Which of the following is banned in Singapore?": [
+                "Chewing Gum",
+                "Alcohol",
+                "Cigarettes",
+                "Pet Chinchillas"
+            ]
+        },
+        {
+            "When was Singapore's last general election?": [
+                "2015",
+                "2011",
+                "2016",
+                "2006"
+            ]
+        },
+        {
+            "Singapore was previously colonized by which country?": [
+                "Great Britain",
+                "Portugal",
+                "Spain",
+                "France"
+            ]
+        },
+        {
+            "What is the name of the Singapore defence force?": [
+                "Singapore Armed Forces",
+                "Singapore Defence Force",
+                "National Defense Force of Singapore",
+                "Singaporean Army"
+            ]
+        },
+        {
+            "What is the capital of Singapore": [
+                "Singapore",
+                "Kuala Lampur",
+                "Orchard",
+                "Dhoby Ghuat"
+            ]
+        },
+        {
+            "What is Singapore's oldest university?": [
+                "National University of Singapore",
+                "Nanyang Technological University",
+                "Singapore Management University",
+                "Singapore University of Technology and Design"
+            ]
+        },
+        
+    ],
+};
